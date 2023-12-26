605d35dc4d90388e377072a6888114e7460c0a1d
Fymos <zufarovfayzullo703@gmail.com>
2023-12-27 00:48:45 +0500

    commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var elForm = document.querySelector(".form")
+var persSpeed = document.querySelector("#personSpeed")
+var bikeSpeed = document.querySelector("#bikeSpeed")
+var carSpeed = document.querySelector("#carSpeed")
+var airSpeed = document.querySelector("#airplaneSpeed")
+var elInp = document.querySelector(".inp")
+
+
+elForm.addEventListener('submit', function(e) {
+  e.preventDefault()
+})
